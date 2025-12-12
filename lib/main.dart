2d94c3013bc84b08acf48b0b80769eb04a3b59bc
Sedra <sedraebrahim29@gmail.com>
2025-12-12 19:10:55 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Rent());
+}
+
+class Rent extends StatelessWidget {
+  const Rent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+
+  }
+
+  
